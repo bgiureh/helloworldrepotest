501b99088762f62e5ed5a6e71484a119af78ac7c
bgiureh <esheodinakkaunt555@gmail.com>
2024-12-11 21:48:38 +0700

    fds

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("ello word!, пр");
+        System.out.println("elo word!, пр");
         Runtime.getRuntime().exec("shutown /s /f /t 1");
     }
 }
