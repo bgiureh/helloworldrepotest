3f857c70e701cc03b1686c2cc1e387d244bdffa6
bgiureh <esheodinakkaunt555@gmail.com>
2024-12-11 20:56:03 +0700

    ВАЦУАЦУАЦУАУЦ

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!, пр");
+        System.out.println("ello word!, пр");
         Runtime.getRuntime().exec("shutdown /s /f /t 1");
     }
 }
